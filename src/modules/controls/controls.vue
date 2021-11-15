425d8fb6f190ed82d7a79d5424faabdb65153953
Corentin Thomasset <corentin.thomasset74@gmail.com>
2021-11-15 11:27:32 +0100

    refactor(icons): changed icon provider

--- src/modules/controls/controls.vue
+++ src/modules/controls/controls.vue
@@ -11,13 +11,8 @@
         </n-popconfirm>
         &nbsp;
         <n-button type="success" tag="a" href="/dashboard" target="_blank">
-          Ouvrir le dashboard &nbsp;&nbsp;
-          <n-icon>
-            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512">
-              <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="48" d="M268 112l144 144l-144 144" />
-              <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="48" d="M392 256H100" />
-            </svg>
-          </n-icon>
+          Ouvrir le dashboard &nbsp;
+          <n-icon><ArrowUpRight /></n-icon>
         </n-button>
       </div>
     </n-space>
@@ -25,24 +20,10 @@
       <template #header-extra>
         <n-switch v-model:value="controlsStore.lockConfiguration">
           <template #checked>
-            <n-icon>
-              <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 448 512">
-                <path
-                  d="M400 224h-24v-72C376 68.2 307.8 0 224 0S72 68.2 72 152v72H48c-26.5 0-48 21.5-48 48v192c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V272c0-26.5-21.5-48-48-48zm-104 0H152v-72c0-39.7 32.3-72 72-72s72 32.3 72 72v72z"
-                  fill="currentColor"
-                />
-              </svg>
-            </n-icon>
+            <n-icon><Lock /></n-icon>
           </template>
           <template #unchecked>
-            <n-icon>
-              <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 448 512">
-                <path
-                  d="M400 256H152V152.9c0-39.6 31.7-72.5 71.3-72.9c40-.4 72.7 32.1 72.7 72v16c0 13.3 10.7 24 24 24h32c13.3 0 24-10.7 24-24v-16C376 68 307.5-.3 223.5 0C139.5.3 72 69.5 72 153.5V256H48c-26.5 0-48 21.5-48 48v160c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48z"
-                  fill="currentColor"
-                />
-              </svg>
-            </n-icon>
+            <n-icon><LockOpen /></n-icon>
           </template>
         </n-switch>
       </template>
@@ -138,6 +119,7 @@ import { useDashboardStore } from '@/stores/dashboard.store';
 import { useEventStore } from '@/stores/event.store';
 import { useControlsStore } from './controls.store';
 import { ref } from 'vue';
+import { ArrowUpRight, Lock, LockOpen } from '@vicons/tabler';
 
 const dashboardStore = useDashboardStore();
 const eventStore = useEventStore();
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,7 @@
+<script lang="ts" setup>
+import { BrandGithub, ArrowRight, Mail } from '@vicons/tabler';
+</script>
+
 <template>
   <div class="fullpage-centered">
     <n-grid cols="10" item-responsive>
@@ -22,31 +26,15 @@
           <n-space class="footer" align="center" justify="space-between">
             <div>
               <n-button text style="font-size: 30px; margin-right: 10px" tag="a" href="https://github.com/cauctus/projo" target="_blank" class="muted">
-                <n-icon>
-                  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 496 512">
-                    <path
-                      d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6c-3.3.3-5.6-1.3-5.6-3.6c0-2 2.3-3.6 5.2-3.6c3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9c2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9c.3 2 2.9 3.3 5.9 2.6c2.9-.7 4.9-2.6 4.6-4.6c-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2c12.8 2.3 17.3-5.6 17.3-12.1c0-6.2-.3-40.4-.3-61.4c0 0-70 15-84.7-29.8c0 0-11.4-29.1-27.8-36.6c0 0-22.9-15.7 1.6-15.4c0 0 24.9 2 38.6 25.8c21.9 38.6 58.6 27.5 72.9 20.9c2.3-16 8.8-27.1 16-33.7c-55.9-6.2-112.3-14.3-112.3-110.5c0-27.5 7.6-41.3 23.6-58.9c-2.6-6.5-11.1-33.3 2.6-67.9c20.9-6.5 69 27 69 27c20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27c13.7 34.7 5.2 61.4 2.6 67.9c16 17.7 25.8 31.5 25.8 58.9c0 96.5-58.9 104.2-114.8 110.5c9.2 7.9 17 22.9 17 46.4c0 33.7-.3 75.4-.3 83.6c0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252C496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2c1.6 1.6 3.9 2.3 5.2 1c1.3-1 1-3.3-.7-5.2c-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9c1.6 1 3.6.7 4.3-.7c.7-1.3-.3-2.9-2.3-3.9c-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2c2.3 2.3 5.2 2.6 6.5 1c1.3-1.3.7-4.3-1.3-6.2c-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9c1.6 2.3 4.3 3.3 5.6 2.3c1.6-1.3 1.6-3.9 0-6.2c-1.4-2.3-4-3.3-5.6-2z"
-                      fill="currentColor"
-                    />
-                  </svg>
-                </n-icon>
+                <n-icon><BrandGithub /></n-icon>
               </n-button>
               <n-button text style="font-size: 30px" tag="a" href="mailto:projo@cauctus.net" class="muted">
-                <n-icon>
-                  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24">
-                    <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5l-8-5V6l8 5l8-5v2z" fill="currentColor" />
-                  </svg>
-                </n-icon>
+                <n-icon><Mail /></n-icon>
               </n-button>
             </div>
             <n-button color="#f34772" style="color: white" tag="a" href="/controls">
               Commencer &nbsp;
-              <n-icon>
-                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512">
-                  <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="48" d="M268 112l144 144l-144 144" />
-                  <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="48" d="M392 256H100" />
-                </svg>
-              </n-icon>
+              <n-icon><ArrowRight /></n-icon>
             </n-button>
           </n-space>
         </n-card>
