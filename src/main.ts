edbc377307463570c980c8bc588ac8733293f7fc
Corentin Thomasset <corentin.thomasset74@gmail.com>
2021-11-23 13:17:50 +0100

    fix(style): added font in assets

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import { router } from './router';
+
 import { naive } from './plugins/naive.plugin';
 import { pinia } from './plugins/pinia.plugin';
 import { plausible } from './plugins/plausible.plugin';
+
+import '@/assets/font.css';
 
 createApp(App).use(pinia).use(router).use(naive).use(plausible).mount('#app');
