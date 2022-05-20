7519ed033f10b05521810431e8e9fcda09c897e4
Corentin Thomasset <corentin.thomasset74@gmail.com>
2022-05-20 18:11:35 +0200

    fix: impro duration clean on import

--- src/utils/upload.ts
+++ src/utils/upload.ts
@@ -12,11 +12,13 @@ export async function fileToJson(file: Blob) {
   return JSON.parse(await readFile(file));
 }
 
-export async function parseImportedImproFile(file: Blob) {
+export async function parseImportedImproFile(file: Blob): Promise<Impro[]> {
   const improFileContent = await fileToJson(file);
 
   if (improFileContent.version === 1) {
-    return improFileContent.impros as Impro[];
+    const fixedImpro = (improFileContent.impros as Impro[]).map(({ duration, ...impro }) => ({ duration: ((duration / 60000) % 60) * 60000, ...impro }));
+
+    return fixedImpro;
   } else {
     throw 'Unsupported version';
   }
